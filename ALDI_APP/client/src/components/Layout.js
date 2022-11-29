90ff5d180747bef4594fb225cc3acae5159c5dd9
Anthony Salerno <anthonysalerno97@gmail.com>
2022-11-29 14:48:48 -0600

    remove unnecessary folders

--- ALDI_APP/client/src/components/Layout.js
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-const Layout = () => {
-    console.log("hey how yall doing layout");
-    // render children of outlet component
-    // could add header/footer on EVERY page, this is the parent to add extra stuff
-    return <Outlet />
-}
-export default Layout;
