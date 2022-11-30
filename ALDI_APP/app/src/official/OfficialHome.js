62552e2c73c9f91d08a7b0c799401d2541ca2ae1
bdelatorre21 <briandelatorr21@gmail.com>
2022-11-30 10:50:40 -0600

    Homepage V2

--- ALDI_APP/app/src/official/OfficialHome.js
+++ ALDI_APP/app/src/official/OfficialHome.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles.css';
 import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import useAuth from '../simple/useAuth';
+import DateAndTime from './DateAndTime';
 import './officialhome.css';
 
 const OfficialHome = () => {
@@ -17,74 +18,111 @@ const OfficialHome = () => {
     }
 
     return (
-            <div className='homepage-main'>
+        <div>
+            <body className="homepage-body">
+            <div className="homepage-container">
                 <div className="homepage-content">
                     <div className="homepage-introduction">
-                        <h2>Welcome Back, {auth.first_name}</h2>
+                        <h2>Welcome Back, Username</h2>
                         <div className="test-container">
                             <div className="date-header">
-                                <p className="date-p">24 Thursday 2022</p>
+                                <p className="date-p"><DateAndTime/></p>
                                 <button className="calendar-button"><img src="https://cdn-icons-png.flaticon.com/512/3217/3217869.png" height={30} width={30} /> </button>
                             </div>
                         </div>
-                        <div className='cards-container'>
-                            <div className="cards jsGridView">
-                                <div className="card-wrapper">
-                                    <div>Request Time Off</div>
-                                    <img src="https://cdn-icons-png.flaticon.com/512/2370/2370264.png" height={50} width={50} className="card-picture" />
+                        <br/><br/>                                               <br/><br/>
+                        <div className="cards jsGridView">
+                            <div className="card-wrapper">
+                                <div className="card">
+                                    <div className="card-header">
+                                    </div>
+                                    <div className="card-content-header">
+                                        <p className="card-info-header">Request Time Off</p>
+                                        <Link to="/statsPage">
+                                            <img src="https://cdn-icons-png.flaticon.com/512/2370/2370264.png" height={50} width={50} className="card-picture" />
+                                        </Link>
+                                    </div>
                                 </div>
-                                <div className="card-wrapper">
-                                    <div>View Time Off Balance</div>
-                                    <img src="https://cdn-icons-png.flaticon.com/512/2117/2117251.png" height={50} width={50} className="card-picture" />
+                            </div>
+                            <div className="card-wrapper">
+                                <div className="card">
+                                    <div className="card-header">
+                                    </div>
+                                    <div className="card-content-header">
+                                        <p className="card-info-header">View Time Off Balance</p>
+                                        <Link to="/statsPage">
+                                            <img src="https://cdn-icons-png.flaticon.com/512/2117/2117251.png" height={50} width={50} className="card-picture" />
+                                        </Link>
+                                    </div>
                                 </div>
-                                <div className="card-wrapper">
-                                        <div>Some Option Here</div>
-                                        <img src="https://cdn-icons-png.flaticon.com/512/57/57108.png" height={50} width={50} className="card-picture" />
+                            </div>
+                            <div className="card-wrapper">
+                                <div className="card">
+                                    <div className="card-header">
+                                    </div>
+                                    <div className="card-content-header">
+                                        <p className="card-info-header"></p>
+                                    </div>
                                 </div>
-                                <div className="card-wrapper">
-                                    <div>Some Option Here</div>
-                                    <img src="https://cdn-icons-png.flaticon.com/512/57/57108.png" height={50} width={50} className="card-picture" />
+                            </div>
+                            <div className="card-wrapper">
+                                <div className="card">
+                                    <div className="card-header">
+                                    </div>
+                                    <div className="card-content-header">
+                                        <p className="card-info-header"></p>
+                                    </div>
                                 </div>
-                                <div className="card-wrapper">
-                                    <div>Some Option Here</div>
-                                    <img src="https://cdn-icons-png.flaticon.com/512/57/57108.png" height={50} width={50} className="card-picture" />
+                            </div>
+                            <div className="card-wrapper">
+                                <div className="card">
+                                    <div className="card-header">
+                                    </div>
+                                    <div className="card-content-header">
+                                        <p className="card-info-header"></p>
+                                    </div>
                                 </div>
-                                <div className="card-wrapper">
-                                    <div>Some Option Here</div>
-                                    <img src="https://cdn-icons-png.flaticon.com/512/57/57108.png" height={50} width={50} className="card-picture" />
+                            </div>
+                            <div className="card-wrapper">
+                                <div className="card">
+                                    <div className="card-header">
+                                    </div>
+                                    <div className="card-content-header">
+                                        <p className="card-info-header"></p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="notification-container">
-                        <div className='notification-section'>
-                            <div className="notification-header">
-                                <p>Your Notifications</p>
-                            </div>
-                            <div className="notifications">
-                                <div className="notify-container">
-                                    <div className="notify-content">
-                                        <div className="not-header">
-                                            <div className="message-alert">New Message</div>
-                                        </div>
-                                        <p className="notification-content-message">
-                                            Your requested day off for December, 21, 2022 to December, 30, 2022 was Declined. Reason: not enough vacation days.
-                                        </p>
+                    <div className="notification-section">
+                        <div className="notification-header">
+                            <p>Your Notifications</p>
+                        </div>
+                        <div className="notifications">
+                            <div className="notify-container">
+                                <div className="notify-content">
+                                    <div className="not-header">
+                                        <div className="message-alert">New Message</div>
                                     </div>
+                                    <p className="notification-content-message">
+                                        Your requested day off for December, 21, 2022 to December, 30, 2022 was Declined. Reason: not enough vacation days.
+                                    </p>
                                 </div>
-                                <div className="notify-container">
-                                    <div className="notify-content">
-                                        <div className="not-header">
-                                            <div className="message-alert">New Message</div>
-                                        </div>
-                                        <p className="notification-content-message">Your Requested Time Off for November, 23, 2022 was Accepted. Type: Sick Day.</p>
+                            </div>
+                            <div className="notify-container">
+                                <div className="notify-content">
+                                    <div className="not-header">
+                                        <div className="message-alert">New Message</div>
                                     </div>
+                                    <p className="notification-content-message">Your Requested Time Off for November, 23, 2022 was Accepted. Type: Sick Day.</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            </body>
+        </div>
     )
 }
 
