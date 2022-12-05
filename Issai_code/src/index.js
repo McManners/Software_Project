92ade2cfdece5007fd947c4efb0f85de19fada29
IssaiN <inunez02@aurora.edu>
2022-12-05 00:13:59 -0600

    Update index.js

--- Issai_code/src/index.js
+++ Issai_code/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'chart.js'
-//import App from './App';
-import Dchart from "./project/Dchart";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'chart.js';
+import 'react-chartjs-2';
+
+import "./Chart_project/CSS/StatsCss.css";
+import "./Chart_project/CSS/ChartTestCss.css";
+//import "./Chart_project/CSS/ChartTextCss.css";
+import App from './App';
+
+import Stats from "./Chart_project/Stats";
+//import ChartText from "./Chart_project/ChartText";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Dchart/>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Stats/>
+    </React.StrictMode>
 );
-
